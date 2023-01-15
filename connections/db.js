77e4bbe4d11e0ffc483b600289af7880c4c615ad
Varun Kumar Bansal <varunbansal326@gmail.com>
2023-01-15 21:47:07 +0530

    temp

--- connections/db.js
+++ connections/db.js
@@ -1,8 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
-const DATABASE_NAME = 'reunion-assignment'
-const CONNECTION_URL = `mongodb+srv://varun:90977@old-store.7hpcw.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`
 
-mongoose.connect(CONNECTION_URL, {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
